fdb927750c7d6755dcc279916ef566d0e27880dd
Leandro Pedicino <leakomvial@gmail.com>
2026-07-06 09:59:45 -0300

    Completar configuración outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,9 @@
 output "cloudfront_url" {
   value = "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"
 }
+
+# Añade esto para ver la URL de tu API al terminar
+output "api_url" {
+  value = "${aws_api_gateway_stage.visitor_stage.invoke_url}/contador"
+  description = "URL para acceder a la función del contador"
+}
